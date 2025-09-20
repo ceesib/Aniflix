b7f852aaf1b9bae48ad8eb12ad9cdecb7876924d
ceejay sibeko <ceejay.sibeko@ibm.com>
2025-09-20 21:07:24 +0200

    removed server logic from main method

---
+++ Backend/simple-http-server/src/main/java/com/core/HttpServerThread.java
@@ -0,0 +1,36 @@
+package com.core;
+
+import java.io.IOException;
+import java.net.*;
+import org.slf4j.*;
+
+import com.sun.net.httpserver.HttpServer;
+
+
+public class HttpServerThread extends Thread{
+    Logger LOGGER = LoggerFactory.getLogger(HttpServer.class);
+    private int port;
+    public HttpServerThread(int port){
+        this.port = port;
+
+    }
+    @Override
+    public void run(){
+        try {
+            ServerSocket serverSocket = new ServerSocket(this.port);
+            LOGGER.info("Server started. Listening on port: {}",this.port);
+
+            while(true){
+                Socket clientSocket = serverSocket.accept();
+                LOGGER.info("New client connected: {} ", clientSocket.getInetAddress());
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                clientHandler.start();
+            }
+            
+        } catch (IOException e) {
+              e.printStackTrace();
+        }
+
+    }
+    
+}
--- Backend/simple-http-server/src/main/java/com/core/SimpleHttpServer.java
+++ Backend/simple-http-server/src/main/java/com/core/SimpleHttpServer.java
@@ -1,38 +1,20 @@
 package com.core;
 
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-
 public class SimpleHttpServer 
 {
-    public static void main( String[] args ){
+    public static void main( String[] args ) throws IOException{
 
         Logger LOGGER = LoggerFactory.getLogger(SimpleHttpServer.class);
-
-        try{
-            
-            var cm = new ConfigurationManager("Backend/simple-http-server/src/main/java/com/core/config.json");
-            var node = cm.readFile();
-            var port =  node.get("port").asInt();
-            ServerSocket serverSocket = new ServerSocket(port);
-            LOGGER.info("Server started. Listening on port: {}",port);
-
-            while(true){
-
-                Socket clientSocket = serverSocket.accept();
-                LOGGER.info("New client connected: {} ", clientSocket.getInetAddress());
-                ClientHandler clientHandler = new ClientHandler(clientSocket);
-                clientHandler.start();
-                
-            }
-        }catch(IOException e){
-            
-            e.printStackTrace();
-        }
+        var cm = new ConfigurationManager("Backend/simple-http-server/src/main/java/com/core/config.json");
+        var node = cm.readFile();
+        var port =  node.get("port").asInt();
+        var httpServerThread = new HttpServerThread(port);
+        httpServerThread.start();
+        
+  
     }
 }
